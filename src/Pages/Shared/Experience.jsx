2b634d65df46ea4210f7b0e7af38989e1c6efb08
mahfuzurrahman99 <mahfuzurrahmanshabbir@gmail.com>
2023-12-12 11:35:22 +0600

    Mahfuz's Portfolio Website

--- src/Pages/Shared/Experience.jsx
+++ src/Pages/Shared/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
         <div className="mt-8">
             <h1 className="font-bold text-2xl mb-3">My Experience</h1>
             <div>
-                <h1 className="text-white"><span className="text-gray-300">Not Yet</span> - None</h1>
+                <h1 className="font-bold"><span className="">Not Yet</span> - None</h1>
             </div>
         </div>
     );
