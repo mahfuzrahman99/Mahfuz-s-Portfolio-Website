bf5b4adc30ad816f1a31ef4691c19068592ecf50
Mahfuzur Rahman Shabbir <mahfuzurrahmanshabbir@gmail.com>
2024-02-19 01:24:06 +0600

    backend url changed

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosSecure = axios.create({
-    baseURL: "https://mahfuz-prtfolio-website-server-side.vercel.app",
+    baseURL: "https://mahfuz-s-portfolio-website-server.vercel.app",
   // baseURL: "http://localhost:5000",
 });
 const useAxiosPublic = () => {
